bc7e2edf43b634e5df7b3aa7fb40970fcf9d29f3
Ryan Friedman <rfriedman@football.com>
2014-11-05 16:48:24 -0800

    changed bad index update in migration

--- db/migrate/20141103083408_update_index_on_users.rb
+++ db/migrate/20141103083408_update_index_on_users.rb
@@ -1,7 +1,5 @@
 class UpdateIndexOnUsers < ActiveRecord::Migration
   def change
-    sql = 'DROP INDEX index_users_on_email'
-    sql << ' ON users' if Rails.env == 'production' # Heroku pg
-    ActiveRecord::Base.connection.execute(sql)
+    remove_index :users, :email
   end
 end
