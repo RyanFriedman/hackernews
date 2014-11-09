bc2b6fced556901e59e8412883ee57e44f398464
Ryan Friedman <rfriedman@football.com>
2014-11-09 11:10:25 -0800

    fixed user update form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,24 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order("karma DESC"). limit(50)
+    @users = User.order("karma DESC").limit(50)
   end
   
   def show
     @user = User.find(params[:id])
   end
   
-  def update
+  def update 
     @user = User.find(params[:id])
-    @user.update_attributes(user_params)
-    redirect_to :action => 'show', :id => @book
+    if @user.update_attributes(user_params)
+      flash[:notice] = "Your changes have been saved!"
+    end
+    redirect_to :action => 'show', :id => @user
   end
   
   private
     
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :bid_amount, :biddable_type, :biddable_id)
+      params.require(:user).permit(:email, :about, :bid_amount, :biddable_type, :biddable_id)
     end
 end
